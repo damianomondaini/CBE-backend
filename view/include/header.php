06593f1fe01437f6b1aab1cf40fd64a9e61a9c77
Damiano <35708577+damianomondaini@users.noreply.github.com>
2020-03-12 10:52:07 +0100

    Edited links

--- view/include/header.php
+++ view/include/header.php
@@ -2,15 +2,15 @@
     <nav>
         <nav class="white" role="navigation">
             <div class="nav-wrapper container">
-                <a href="http://localhost/CBE-backend/index.php" class="brand-logo"><img id="logo_cpnv" alt="logo_cpnv" src="src\public\img\logo_cpnv.png"></a>
+                <a href="index.php" class="brand-logo"><img id="logo_cpnv" alt="logo_cpnv" src="src\public\img\logo_cpnv.png"></a>
 
                 <ul class="hide-on-med-and-down right">
                         <li><a href="#" class="cpnvgreen">Box natel</a></li>
-                        <li><a href="http://localhost/CBE-backend/index.php?req=product&role=2&productId=1" class="cpnvgreen">Cartes de visites</a></li>
+                        <li><a href="index.php?req=product&role=2&productId=1" class="cpnvgreen">Cartes de visites</a></li>
                         <li><a href="#" class="cpnvgreen">Salle multimédia</a></li>
-                        <li><a href="http://localhost/CBE-backend/index.php?req=dashboard&role=1" class="cpnvgreen">Dashboard student</a></li>
-                        <li><a href="http://localhost/CBE-backend/index.php?req=dashboard&role=2" class="cpnvgreen">Dashboard customer</a></li>
-                        <li><a href="http://localhost/CBE-backend/index.php?req=dashboard&role=3" class="cpnvgreen">Dashboard admin</a></li>
+                        <li><a href="index.php?req=dashboard&role=1" class="cpnvgreen">Dashboard student</a></li>
+                        <li><a href="index.php?req=dashboard&role=2" class="cpnvgreen">Dashboard customer</a></li>
+                        <li><a href="index.php?req=dashboard&role=3" class="cpnvgreen">Dashboard admin</a></li>
                         <li><a href="#" class="cpnvgreen"><i class="material-icons">person</i></a></li>
                 </ul>
 
